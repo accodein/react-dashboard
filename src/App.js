43e1eed4b7ce7e9819fa33f10b7ff3af3efd8308
manipratap2 <mannipratap@gmail.com>
2021-04-15 18:47:41 +0530

    initialized sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./contents/navbar";
+import Sidebar from "./contents/sidebar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Sidebar />
     </div>
   );
 }
