d43ac77bb435c260a7623422eeec4cf56c775179
manipratap2 <mannipratap@gmail.com>
2021-04-15 18:50:22 +0530

    tested sidebar

--- src/contents/sidebar.js
+++ src/contents/sidebar.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import classes from "./styles/sidebar.module.scss";
 
 class Sidebar extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.Sidebar}>
         <h1>Hello</h1>
       </div>
     );
