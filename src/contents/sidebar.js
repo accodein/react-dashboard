898c38daca4e8aa51d16e6e44d457ca85663e6e6
manipratap2 <mannipratap@gmail.com>
2021-04-15 19:56:28 +0530

    added sidebar menu

--- src/contents/sidebar.js
+++ src/contents/sidebar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./styles/sidebar.module.scss";
 import Dog from "../assets/labrador.jpg";
+import { FaChartPie, FaTicketAlt } from "react-icons/fa";
 
 class Sidebar extends Component {
   render() {
@@ -8,7 +9,20 @@ class Sidebar extends Component {
       <div className={classes.MainSidebar}>
         <div className={classes.Sidebar}>
           <div className={classes.Logo}>Accode Dashboard</div>
+          <div>
+            <ul className={classes.Menu}>
+              <li>
+                <FaChartPie />
+                &emsp; &emsp;<span>Overview</span>
+              </li>
+              <li>
+                <FaTicketAlt />
+                &emsp; &emsp;<span>Tickets</span>
+              </li>
+            </ul>
+          </div>
         </div>
+
         <div className={classes.User}>
           <p>Blacky</p>
           <img src={Dog} alt="User" />
