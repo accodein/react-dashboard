b618b3019f318c4a8bfd7975abccaf8606a4f026
manipratap2 <mannipratap@gmail.com>
2021-04-19 17:28:33 +0530

    introduced charts section

---
+++ src/contents/charts.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import classes from "./styles/charts.module.scss";
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [
+          "Boston",
+          "Worcester",
+          "Springfield",
+          "Lowell",
+          "Cambridge",
+          "New Bedford",
+        ],
+        datasets: [
+          {
+            label: "Population",
+            data: [9999, 8888, 7777, 7777, 8888, 9999],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)",
+            ],
+            fill: false,
+            pointRadius: 0,
+          },
+          {
+            label: "Control",
+            data: [1111, 8888, 2222, 7777, 3333, 9999],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)",
+            ],
+            fill: false,
+            pointRadius: 0,
+          },
+        ],
+      },
+    };
+  }
+
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: "right",
+  };
+
+  render() {
+    return (
+      <>
+        <div className={classes.Chart}>
+          <Line
+            data={this.state.chartData}
+            options={{
+              title: {
+                display: this.props.displayTitle,
+                text: "testing Chart",
+                fontSize: 25,
+              },
+              legend: {
+                display: this.props.displayLegend,
+                position: this.props.legendPosition,
+              },
+            }}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+export default Chart;
--- src/contents/overview.js
+++ src/contents/overview.js
@@ -1,90 +1,20 @@
-import React, { Component } from "react";
-import { Line } from "react-chartjs-2";
+import React from "react";
 
 import classes from "./styles/overview.module.scss";
 import SectionNavbar from "./sectionNavbar";
+import SectionCards from "./sectionCards";
+import Chart from "./charts";
 
-class Overview extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartData: {
-        labels: [
-          "Boston",
-          "Worcester",
-          "Springfield",
-          "Lowell",
-          "Cambridge",
-          "New Bedford",
-        ],
-        datasets: [
-          {
-            label: "Population",
-            data: [9999, 8888, 7777, 7777, 8888, 9999],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.6)",
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)",
-              "rgba(75, 192, 192, 0.6)",
-              "rgba(153, 102, 255, 0.6)",
-              "rgba(255, 159, 64, 0.6)",
-              "rgba(255, 99, 132, 0.6)",
-            ],
-            fill: false,
-            pointRadius: 0,
-          },
-          {
-            label: "Control",
-            data: [1111, 8888, 2222, 7777, 3333, 9999],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.6)",
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)",
-              "rgba(75, 192, 192, 0.6)",
-              "rgba(153, 102, 255, 0.6)",
-              "rgba(255, 159, 64, 0.6)",
-              "rgba(255, 99, 132, 0.6)",
-            ],
-            fill: false,
-            pointRadius: 0,
-          },
-        ],
-      },
-    };
-  }
+const overview = (props) => {
+  return (
+    <>
+      <div className={classes.OverviewBody}>
+        <SectionNavbar>Overview</SectionNavbar>
+        <SectionCards />
+        <Chart />
+      </div>
+    </>
+  );
+};
 
-  static defaultProps = {
-    displayTitle: true,
-    displayLegend: true,
-    legendPosition: "right",
-  };
-
-  render() {
-    return (
-      <>
-        <div className={classes.OverviewBody}>
-          <SectionNavbar>Overview</SectionNavbar>
-
-          <div className={classes.Chart}>
-            <Line
-              data={this.state.chartData}
-              options={{
-                title: {
-                  display: this.props.displayTitle,
-                  text: "testing Chart",
-                  fontSize: 25,
-                },
-                legend: {
-                  display: this.props.displayLegend,
-                  position: this.props.legendPosition,
-                },
-              }}
-            />
-          </div>
-        </div>
-      </>
-    );
-  }
-}
-
-export default Overview;
+export default overview;
