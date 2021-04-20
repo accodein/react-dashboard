b704d164464667314b5d1096b2657684d542a95b
manipratap2 <mannipratap@gmail.com>
2021-04-20 09:55:31 +0530

    modified chart

--- src/contents/charts.js
+++ src/contents/charts.js
@@ -1,60 +1,56 @@
 import React, { Component } from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import classes from "./styles/charts.module.scss";
 
 class Chart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chartData: {
-        labels: [
-          "Boston",
-          "Worcester",
-          "Springfield",
-          "Lowell",
-          "Cambridge",
-          "New Bedford",
-        ],
-        datasets: [
-          {
-            label: "Population",
-            data: [9999, 8888, 7777, 7777, 8888, 9999],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.6)",
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)",
-              "rgba(75, 192, 192, 0.6)",
-              "rgba(153, 102, 255, 0.6)",
-              "rgba(255, 159, 64, 0.6)",
-              "rgba(255, 99, 132, 0.6)",
-            ],
-            fill: false,
-            pointRadius: 0,
-          },
-          {
-            label: "Control",
-            data: [1111, 8888, 2222, 7777, 3333, 9999],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.6)",
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)",
-              "rgba(75, 192, 192, 0.6)",
-              "rgba(153, 102, 255, 0.6)",
-              "rgba(255, 159, 64, 0.6)",
-              "rgba(255, 99, 132, 0.6)",
-            ],
-            fill: false,
-            pointRadius: 0,
-          },
-        ],
-      },
+      chartData: {},
     };
+  }
+
+  componentDidMount() {
+    var ctx = document.getElementById("canvas").getContext("2d");
+    var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+    gradient.addColorStop(0, "rgba(229, 239, 255, 1)");
+    gradient.addColorStop(1, "#FFFFFF");
+    const newData = {
+      labels: [
+        "Boston",
+        "Worcester",
+        "Springfield",
+        "Lowell",
+        "Cambridge",
+        "New Bedford",
+      ],
+      datasets: [
+        {
+          label: "Population",
+          data: [4111, 5555, 7777, 3777, 8888, 3999],
+          fill: false,
+          pointRadius: 0,
+          borderColor: "rgba(255, 159, 64, 0.6)",
+          borderWidth: 1,
+        },
+        {
+          label: "Control",
+          data: [1111, 8888, 2222, 7777, 3333, 9999],
+          fill: true,
+          backgroundColor: gradient,
+          pointRadius: 0,
+          borderColor: "#3751FF",
+          borderWidth: 1,
+        },
+      ],
+    };
+    this.setState({ chartData: newData });
   }
 
   static defaultProps = {
     displayTitle: true,
     displayLegend: true,
-    legendPosition: "right",
+    legendPosition: "top",
   };
 
   render() {
@@ -62,7 +58,8 @@ class Chart extends Component {
       <>
         <div className={classes.Container}>
           <div className={classes.Chart}>
-            <Doughnut
+            <Line
+              id="canvas"
               data={this.state.chartData}
               options={{
                 title: {
@@ -74,6 +71,16 @@ class Chart extends Component {
                 legend: {
                   display: this.props.displayLegend,
                   position: this.props.legendPosition,
+                  align: "end",
+                },
+                scales: {
+                  xAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                    },
+                  ],
                 },
               }}
             />
