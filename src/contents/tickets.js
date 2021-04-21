71207453fd47e13b215424d205e8db0d1ca03063
manipratap2 <mannipratap@gmail.com>
2021-04-21 11:43:42 +0530

    adding buttons

--- src/contents/tickets.js
+++ src/contents/tickets.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./styles/tickets.module.scss";
 import SectionNavbar from "./sectionNavbar";
+import Button from "./button";
 
 const tickets = () => {
   return (
     <div className={classes.TicketsBody}>
       <SectionNavbar>Tickets</SectionNavbar>
+      <Button btnType="Urgent">New</Button>
     </div>
   );
 };
